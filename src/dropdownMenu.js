1432847df850be29510fe26655097246884929bb
WebWorflowSolutions99 <83876343+WebWorflowSolutions99@users.noreply.github.com>
2022-01-08 16:52:02 -0600

    make a mobile menu

--- src/dropdownMenu.js
+++ src/dropdownMenu.js
@@ -2,22 +2,24 @@
 //call once at page initialization
 export default function dropdownMenu(mainMenuButton, subMenuDiv) {
   //set visibility off initally:
-  subMenuDiv.style.visibility = "hidden";
+  console.log(mainMenuButton);
+  console.log(subMenuDiv);
+  subMenuDiv.style.display = "none";
 
   // call back to toggle sub menu visibility when the main menu button is clicked
   mainMenuButton.addEventListener("mouseover", (e) => {
-    subMenuDiv.style.visibility = "visible";
+    subMenuDiv.style.display = "block";
   });
 
   subMenuDiv.addEventListener("mouseleave", (e) => {
-    subMenuDiv.style.visibility = "hidden";
+    subMenuDiv.style.display = "none";
   });
 
   mainMenuButton.addEventListener("click", (e) => {
-    if (subMenuDiv.style.visibility === "visible") {
-      subMenuDiv.style.visibility = "hidden";
+    if (subMenuDiv.style.display === "block") {
+      subMenuDiv.style.display = "none";
     } else {
-      subMenuDiv.style.visibility = "visible";
+      subMenuDiv.style.display = "block";
     }
   });
 }
--- src/index.js
+++ src/index.js
@@ -2,28 +2,55 @@ import "./style.css";
 import index from "./index.html";
 import dropdownMenu from "./dropdownMenu";
 import addIconSrc from "./img/add_circle_black_24dp.svg";
+import menuIconSrc from "./img/menu_black_24dp.svg";
 
 // Generate navigation bar
-/*
-function navBar(callback) {
+
+function navBar(title) {
   const nav = document.createElement("div");
   nav.classList.add("nav");
+  nav.classList.add("mobile");
 
-  const addIcon = new Image();
-  addIcon.src = addIconSrc;
-  nav.appendChild(addIcon);
+  const h1 = document.createElement("h1");
+  h1.innerHTML = title;
+  h1.classList.add("navTitle");
+  nav.appendChild(h1);
 
+  const menuIcon = new Image();
+  menuIcon.src = menuIconSrc;
+  menuIcon.classList.add("mainMenuMobile");
+  nav.appendChild(menuIcon);
+
+  /*
   addIcon.addEventListener("click", (e) => {
     callback();
   });
-
-  let body = document.querySelector("body");
+  */
+  const body = document.querySelector("body");
   body.appendChild(nav);
+
+  const dropdown = document.createElement("div");
+  dropdown.innerHTML = "<a>SUB MENU 1</a> <a>SUB MENU 2</a> <a>SUB MENU 3</a>";
+  dropdown.classList.add("dropdownMenuMobile");
+  body.appendChild(dropdown);
 }
-*/
+
 dropdownMenu(
   document.querySelector(".mainMenuButton"),
   document.querySelector(".dropdownMenu")
 );
 
+navBar("SOME SITE");
+
+dropdownMenu(
+  document.querySelector(".mainMenuMobile"),
+  document.querySelector(".dropdownMenuMobile")
+);
+
 //add stuff here
+const content = document.createElement("div");
+content.innerHTML =
+  "<p> orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>";
+
+const body = document.querySelector("body");
+body.appendChild(content);
